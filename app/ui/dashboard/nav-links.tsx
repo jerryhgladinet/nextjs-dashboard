ed3d25bc9ce29ba5a7e5719668a2e1a0ffc2eb3f
Jerry Huang <jerryh@centrestack.com>
2024-10-03 15:02:34 -0400

    update one file

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-
+// testing
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -26,7 +26,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  
+
   return (
     <>
       {links.map((link) => {
